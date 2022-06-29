cd085b6c2da7bf239f14ccfbea80b57c871c4fc7
ZQF <946980557@qq.com>
2022-06-29 14:53:16 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ZQF
+ * @create 2022-06-29 14:50
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
